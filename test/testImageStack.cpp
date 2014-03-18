e2134487a28908d743e250207b525334c224d817
Javier Celaya <jcelaya@gmail.com>
2014-03-18 20:29:24 +0100

    US1: Normalize test cases

--- test/testImageStack.cpp
+++ test/testImageStack.cpp
@@ -1,17 +1,18 @@
-#include "../ImageStack.hpp"
+#include "../Image.hpp"
+#include <boost/test/unit_test.hpp>
 using namespace hdrmerge;
 
-int main(int argc, char * argv[]) {
-    Image e1(argv[1]);
+static const char * image1 = "test/sample1.dng";
+static const char * image2 = "test/sample2.dng";
+
+BOOST_AUTO_TEST_CASE( image_load ) {
+    Image e1(image1);
     e1.dumpInfo();
     Image::LoadResult result = e1.load(nullptr);
-    if (result == Image::LOAD_SUCCESS) {
-        e1.dumpInfo();
-        Image e2(argv[2]);
-        result = e2.load(&e1);
-        if (result == Image::LOAD_SUCCESS) {
-            e2.dumpInfo();
-        }
-    }
-    return result;
+    BOOST_REQUIRE_EQUAL(result, Image::LOAD_SUCCESS);
+    e1.dumpInfo();
+    Image e2(image2);
+    result = e2.load(&e1);
+    BOOST_REQUIRE_EQUAL(result, Image::LOAD_SUCCESS);
+    e2.dumpInfo();
 }

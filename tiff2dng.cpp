83d210db885af313f1dbd91f7880fa8554081bdf
Alexander <Brock.Alexander@web.de>
2020-02-21 00:49:25 +0100

    Fix constants

--- tiff2dng.cpp
+++ tiff2dng.cpp
@@ -9,6 +9,10 @@
 void convert(std::string filename) {
     std::cout << "reading input file " << filename << std::endl;
     cv::Mat_<uint16_t> im = cv::imread(filename, cv::IMREAD_UNCHANGED);
+    double minval = 0;
+    double maxval = 0;
+    cv::minMaxIdx(im, &minval, &maxval);
+    im *= 65535.0/maxval;
     int const width = im.cols;
     int const height = im.rows;
     std::cout << "size: " << im.size() << std::endl;
@@ -21,17 +25,31 @@ void convert(std::string filename) {
     }
     cv::Mat preview;
     cv::cvtColor(im, preview, cv::COLOR_BayerBG2BGR);
-    cv::imwrite(filename + "-preview.tif", preview);
-    hdrmerge::DngFloatWriter writer;
-    writer.setPreviewWidth(100); // This is important, otherwise
-    hdrmerge::RawParameters params;
+    preview.convertTo(preview, CV_8UC3, 1.0/256);
+    cv::resize(preview, preview, cv::Size(), .5, .5);
+    //cv::imwrite(filename + "-preview.tif", preview);
+    cv::cvtColor(preview, preview, cv::COLOR_BGR2RGB);
+    hdrmerge::RawParameters params((filename + ".dng").c_str());
     params.height = params.rawHeight = height;
     params.width = params.rawWidth = width;
     params.fileName = filename.c_str();
     params.isoSpeed = 100;
-    params.shutter = 100;
+    params.shutter = .1;
     params.aperture = 2.8;
-    //params.FC.setPattern(2);
+    params.colors = 3;
+    params.black = 0;
+
+
+    //QImage q_preview = hdrmerge::ImageIO::renderPreview(arr, params, 1.0);
+    QImage q_preview = QImage(preview.data, preview.size().width, preview.size().height, QImage::Format_RGB888);
+    std::cout << "q_preview width / height: " << q_preview.width() << " / " << q_preview.height() << std::endl;
+    hdrmerge::DngFloatWriter writer;
+    writer.setBitsPerSample(16);
+    writer.setPreviewWidth(q_preview.width());
+    writer.setPreview(q_preview);
+
+
+    params.FC.setPattern(3031741620);
 
     std::cout << "Writing result" << std::endl;
     writer.write(std::move(arr), params, (filename + ".dng").c_str());
@@ -42,7 +60,6 @@ int main(int argc, char ** argv) {
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " input files..." << std::endl;
     }
-
 
     for (int ii = 1; ii < argc; ++ii) {
         convert(argv[ii]);
